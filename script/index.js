3521104f3193f703ecb75b65bc6eadd2f2f86c76
Michael <60814385+pokemzed@users.noreply.github.com>
2022-12-28 22:52:31 +0300

    Finished version of functionality

--- script/index.js
+++ script/index.js
@@ -1,41 +1,64 @@
 const API_URL = 'https://kinopoiskapiunofficial.tech/api/v2.2/films'
 const API_SEARCH_URL = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword='
 
-fetch('https://kinopoiskapiunofficial.tech/api/v2.2/films', {
-    method: 'GET', 
-    headers: { 
-        'X-API-KEY': '0da124d8-8343-4211-b3fe-4cb56d369d0c', 
-        'Content-Type': 'application/json', 
-    }, 
-}) 
-    .then(res => res.json()) 
-    .then(json => gettingElementsFromApi(json)) 
-    .catch(err => console.log(err))
+
+
+const fetching = async (url) => {
+    const res = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'X-API-KEY': '0da124d8-8343-4211-b3fe-4cb56d369d0c',
+            'Content-Type': 'application/json'
+        }
+    })
+    const json = await res.json()
+    gettingElementsFromApi(json)
+}
+fetching(API_URL)
 
 
 function gettingElementsFromApi(json){
-    const catalog = json.items //Array
+    let catalog = json.items //Array
+        catalog ? catalog : catalog = json.films //Проверка полученного JSON
+
+    //Очищаем каталог
     document.querySelector('.catalog').innerHTML = ''
+
     catalog.map(el => {
         const card = markup(el)
         document.querySelector('.catalog').insertAdjacentHTML('beforeend', card)
     }) 
 }
 
+
+//Функция на получение цвета, исходя из значения рейтинга
+const getColorRating = (val) => {
+    return val > 9 ? val = 'review-green' : val > 6 && val < 9 ? 'review-orange' : 'review-red'
+}
+
+//Вырисовка разметки
 const markup = (info) => {
     //Destructuring
     const {
         kinopoiskId: id,
         nameRu: name,
         posterUrlPreview: image,
-        ratingKinopoisk: rating,
+        ratingKinopoisk: rate,
+        rating,
         genres
     } = info
+
+    //Проверка рейтинга в полученном JSON
+    let ratingFilm = rate === undefined ? rating : rate
+        ratingFilm == 'null' ? ratingFilm = '' : ratingFilm //Проверка на null
 
     return html = `
         <div class="catalog-card">
             <div class="card-image" style="background-image: url('${image}');">
-                <div class="review">${rating}</div>
+                ${ratingFilm && (`
+                <div class="review ${getColorRating(ratingFilm)}">${ratingFilm}</div>
+                `)
+            }
             </div>
             <div class="card-text">
                 <h3>${name}</h3>
@@ -44,3 +67,11 @@ const markup = (info) => {
         </div>
     `
 }
+
+
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault()
+    const inputResp = document.querySelector('.header-search')
+    fetching(`${API_SEARCH_URL}${inputResp.value}`)
+    inputResp.value = ''
+})
